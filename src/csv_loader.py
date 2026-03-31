820f4652e9041af8a7225dd24b17cf9ffe8d0a4f
laurenmonahan16 <laurenmon54@gmail.com>
2026-03-31 11:57:14 -0400

    loader and schema manager implemented

--- src/csv_loader.py
+++ src/csv_loader.py
@@ -1,34 +1,136 @@
 import pandas
+import numpy as np
 import sqlite3
+import logging
+from schema_manager import SchemaManager
+
+logging.basicConfig(filename='error_log.txt', level=logging.WARNING)
 
 class csvLoader: 
 
-    def load_csv(filepath: str): 
+    def __init__(self, conn):
+        self.conn = conn
+        self.cur = conn.cursor()
+
+    def load_csv(self, filepath: str): 
         """
         read the csv, return dataframe
         """
-        return pandas.read_csv(filepath)
+        return pandas.read_csv(filepath, sep=None, engine="python" )
 
-    def validate_data(df: pandas.DataFrame) -> bool:
+    def validate_data(self, df: pandas.DataFrame):
         """
-        validation of data before its ingested 
+        validation of data before its ingested **raises errors/logs warnings, does not change data
         """
-        pass
+        # check for empty df
+        if df.empty: 
+            raise ValueError("CSV is empty")
+    
+        # check for empty entries
+        null_entries = df.reset_index(drop=True).isnull()
+        for col in null_entries.columns:
+            for row, is_null in null_entries[col].items():
+                if is_null:
+                    #log empty entry to error.txt
+                    logging.warning(f"Missing value at row {row}, column '{col}'")               
 
-    def get_csv_schema(df: pandas.DataFrame) -> dict[str, str]:
+        # check for empty column names
+        for i, col in enumerate(df.columns):
+            if str(col).strip() == "":
+                raise ValueError(f"Empty column name found at index {i}")
+            elif str(col).startswith("Unnamed"):
+                raise ValueError(f"Unnamed column found at index {i}")
+
+        # check for duplicate column names
+        if df.columns.duplicated().any(): 
+            duplicates = df.columns[df.columns.duplicated()].tolist()
+            raise ValueError(f"Duplicate column names: {duplicates}")
+
+    def get_csv_schema(seld, df: pandas.DataFrame) -> dict[str, str]:
         """
         map csv column -> SQL type (TEXT, INTEGER, REAL)
         """
-        pass
+        type_map = {}
 
-    def insert_rows(conn, df: pandas.DataFrame, table_name: str) -> None:
+        for column, dtype in df.dtypes.items():
+            if "int" in str(dtype):
+                type_map[column] = "INTEGER"
+            elif  "float" in str(dtype): 
+                type_map[column] = "REAL"
+            else:
+                type_map[column] = "TEXT"
+
+        return type_map
+
+    def insert_rows(self, df: pandas.DataFrame, table_name: str) -> None:
         """
         insert DataFrame rows into an existing table
         """
-        pass
+        col_names = df.columns.tolist()
+        col_names_as_string = ", ".join(f'"{col}"' for col in col_names)
+        temp_values = ", ".join(["?"]*len(col_names))
 
-    def ingest(conn, filepath: str, table_name: str, schema_manager) -> None:
+        #build insert command
+        sql_insert = f'INSERT INTO {table_name} ({col_names_as_string}) VALUES ({temp_values})'
+        
+        #cleanup rows for data insertion
+        rows = []
+
+        for row in df.itertuples(index=False, name=None):
+            # if empty entry in csv, replace with "None" so sql can read it 
+            clean_row = []
+            for entry in row:
+                if pandas.isna(entry):
+                    clean_row.append(None)
+                else:
+                    clean_row.append(entry)
+            clean_row = tuple(clean_row)
+            rows.append(clean_row)
+        
+        # execute insertion
+        self.conn.executemany(sql_insert, rows)
+        self.conn.commit()
+
+       
+
+    def ingest(self, filepath: str, table_name: str, schema_manager: SchemaManager) -> None:
         """
         executes: load -> validate -> check schema -> create or append -> insert
         """
-        pass
+        # load the csv 
+        df = self.load_csv(filepath)
+
+        # validate data
+        try:
+            self.validate_data(df)
+        except ValueError as e:
+            print(f"Validation error: {e}")
+
+        # get schema of the csv
+        csv_schema = self.get_csv_schema(df)
+
+        # check if table already exists
+        name_exists = schema_manager.table_already_exists(table_name)
+
+        # if exists, check if schemas match
+        if name_exists:
+            existing_schema = schema_manager.get_table_schema(table_name)
+ 
+            schema_matches = schema_manager.schemas_match(csv_schema, existing_schema)
+
+            if schema_matches:
+                # append data to existing table 
+                self.insert_rows(df, table_name)
+            else: 
+                print(f"Schemas dont match for table by name {table_name}")
+                # later cli should prompt user on what they want to do
+                # 1) do u want to rename the table? 
+                # 2) overwrite the one that exists by same name but the schema doesnt match?
+                # 3) skip ingestion entirely
+        else:
+            #no table by that name exists yet, create new table & insert data
+        
+            create_statement = schema_manager.generate_create_statement(table_name, csv_schema)
+            schema_manager.execute_create(create_statement)
+
+            self.insert_rows(df, table_name)
--- src/schema_manager.py
+++ src/schema_manager.py
@@ -1,3 +1,4 @@
+import sqlite3
 
 class SchemaManager: 
     """
@@ -5,34 +6,61 @@ class SchemaManager:
     """
     def __init__(self, conn):
         self.conn = conn
+        self.cur = self.conn.cursor()
 
     def discover_existing_tables(self) -> list[str]:
         """
         returns a list of all table names in the database
         """
-        pass
+    
+        self.cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+        tables_tuple=(self.cur.fetchall())
+
+        tables_list = []
+        # extract only table name from each tuple
+        for item in tables_tuple:
+            tables_list.append(item[0])
+        
+        return tables_list
 
     def get_table_schema(self, table_name: str) -> dict[str, str]:
         """
-        - inspect column names and data types
-
         returns column name -> type mapping for the given table
         """      
-        pass
-    
-    def get_all_schema(self) -> dict[str, dict[str, str]]:
-        """
-        inspect column names and data types
 
+        self.cur.execute(f"PRAGMA table_info('{table_name}')")
+        info=self.cur.fetchall() #each row in info is a tuple- index 1 is col name and index 2 is type
+
+        type_map={}
+        for item in info:
+            type_map[item[1]]= item[2]
+
+        return type_map
+
+    def get_all_schemas(self) -> dict[str, dict[str, str]]:
+        """
         returns schemas for all tables in the database
         """      
-        pass
+        name_to_schema_map={}
+
+        for item in self.discover_existing_tables():
+            name_to_schema_map[item] = self.get_table_schema(item)
+
+        return name_to_schema_map
 
     def table_already_exists(self, table_name: str) -> bool:
         """
         is there a table by this name that already exists in the database?
         """
-        pass
+        result = False
+
+        existing_tables = self.discover_existing_tables()
+
+        for item in existing_tables:
+            if table_name == item:
+                result=True 
+        
+        return result
 
     def schemas_match(self, new_schema:dict, existing_schema:dict) -> bool:
         """ 
@@ -40,11 +68,45 @@ class SchemaManager:
 
         if true, append data
         if false, create new table
+
+        - Prompt user on schema conflict: overwrite, rename, or skip
+        - Log to error.txt
+
         """
-        pass
+        
+        # remove id column from existing schema
+        existing_schema_filtered = {}
+
+        for column_name, type in existing_schema.items():
+            if column_name != "id":
+                existing_schema_filtered[column_name] = type
+
+        # check if column names in new table match column names of existing table 
+        if set(new_schema.keys()) != set(existing_schema_filtered.keys()):
+            return False
+        
+        # check if data type in new_schema matches type in existing_schema
+        for col in new_schema:
+            if new_schema[col] != existing_schema_filtered[col]:
+                return False
+
+        return True
 
     def generate_create_statement(self, table_name: str, schema: dict[str, str]) -> str:
         """
         Returns the CREATE TABLE SQL string
         """
-        pass
+        name_and_type=[]
+        for name, type in schema.items():
+            name_and_type.append(f'"{name}" {type}')
+
+
+        combined_string = ", ".join(name_and_type)
+        statement= f'CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY AUTOINCREMENT, {combined_string})'
+
+        return statement
+    
+    def execute_create(self, create_command:str):
+
+       self.cur.execute(f'{create_command}')
+       self.conn.commit()
